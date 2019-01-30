3dccd15ae47b7d9a625cb847857d0f505b010109
Bea Huszár <husz.beata@gmail.com>
2019-01-30 16:08:17 +0100

    notes

---
+++ appsettings/appsettings/Model/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppSettingsDemo.Model
+{
+    public class AppSettings
+    {
+        public string Environtment { get; set; }
+    }
+}
